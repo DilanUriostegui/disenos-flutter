2b08c69d9f093976f8546b0fbcc43eced313b9a8
Dilan <duriostegui@asgbpm.com>
2021-04-16 19:05:28 -0500

    Tercer diseño implementando Gradientes, Statusbar, BottomNavigationBar,Table y TableRow y Blur

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:disenos/screens/basic_design.dart';
 import 'package:disenos/screens/scroll_design.dart';
+import 'package:disenos/screens/home_screen.dart';
+import 'package:flutter/services.dart';
+
 
 
  
@@ -10,13 +13,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'scroll_screen' ,
+      initialRoute: 'home_screen' ,
+      theme: ThemeData.dark(),
       routes: {
-        'basic_design': ( _ ) => BasicDesignScreen(),
-        'scroll_screen': ( _ ) => ScrollScreen(),
+        'basic_design'  : ( _ ) => BasicDesignScreen(),
+        'scroll_screen' : ( _ ) => ScrollScreen(),
+        'home_screen'   : ( _ ) => HomeScreen(),
       },
     );
   }
